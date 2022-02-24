114a7db63a704f10073188e03f6f8cbc20412e24
anupam.vedurmudi@ptb.de <anupam.vedurmudi@ptb.de>
2022-02-24 08:58:17 +0100

    rerun of hyperopt for no scaling

--- run_zema_script.sh
+++ run_zema_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+SCALING=${1} # use arg $1 for scaling. Default 1, i.e scale dataset 
 . ~/zema-ml/venv/bin/activate
-python ~/zema-ml/zema_generate_model.py 1
+python ~/zema-ml/zema_generate_model.py $SCALING
